3322d44bbc955194637e1e2a320f0bd4cbd683fa
Dominik Wagner <dom@codingmonkeys.de>
2013-03-27 16:03:04 +0100

    Initial Commit

---
+++ sightsnap/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  sightsnap
+//
+//  Created by Dominik Wagner on 27.03.13.
+//  Copyright (c) 2013 Dominik Wagner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
