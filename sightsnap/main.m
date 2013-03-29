eb5463e54ab012d4c17894a3a0ae4ca05bd756fd
Dominik Wagner <dom@codingmonkeys.de>
2013-03-29 11:23:34 +0100

    [CHANGE] made video device switch work

--- sightsnap/main.m
+++ sightsnap/main.m
@@ -22,9 +22,10 @@ int main(int argc, const char * argv[]) {
         // Arguments setup
         FSArgumentSignature
         *list = [FSArgumentSignature argumentSignatureWithFormat:@"[-l --listDevices]"],
+        *time = [FSArgumentSignature argumentSignatureWithFormat:@"[-t --time]="],
         *device = [FSArgumentSignature argumentSignatureWithFormat:@"[-d --device]="],
         *help = [FSArgumentSignature argumentSignatureWithFormat:@"[-h --help]"];
-        NSArray * signatures = @[list,device, help];
+        NSArray * signatures = @[list,device,time,help];
         FSArgumentPackage * package = [[NSProcessInfo processInfo] fsargs_parseArgumentsWithSignatures:signatures];
         NSString *outputFilename = @"sightsnap.jpg";
         if ([[package uncapturedValues] count] > 0) {
@@ -34,21 +35,40 @@ int main(int argc, const char * argv[]) {
             printf("sightsnap\n\n");
             printf("%s", [[list descriptionForHelp:2 terminalWidth:80] UTF8String]);
             printf("%s", [[device descriptionForHelp:2 terminalWidth:80] UTF8String]);
+            printf("%s", [[time descriptionForHelp:2 terminalWidth:80] UTF8String]);
             printf("%s", [[help descriptionForHelp:2 terminalWidth:80] UTF8String]);
             printf("\n");
             printf("created by @monkeydom\n");
         } else {
             TCMCaptureManager *captureManager = [TCMCaptureManager captureManager];
             if ([package booleanValueForSignature:list]) {
-                puts([[NSString stringWithFormat:@"Video Devices:\n%@",[captureManager.availableVideoDevices componentsJoinedByString:@"\n"]] UTF8String]);
+                puts([[NSString stringWithFormat:@"Video Devices:\n%@",[[captureManager.availableVideoDevices valueForKeyPath:@"localizedDisplayName"] componentsJoinedByString:@"\n"]] UTF8String]);
             } else {
+                
                 QTCaptureDevice *videoDevice = [captureManager.availableVideoDevices lastObject];
+                NSString *deviceString = [package firstObjectForSignature:device];
+                if (deviceString) {
+                    NSString *searchString = deviceString.lowercaseString;
+                    BOOL foundDevice = NO;
+                    for (QTCaptureDevice *device in captureManager.availableVideoDevices) {
+                        NSString *candidateString = [device.localizedDisplayName lowercaseString];
+                        if ([candidateString rangeOfString:searchString].location != NSNotFound) {
+                            foundDevice = YES;
+                            videoDevice = device;
+                            break;
+                        }
+                    }
+                    if (!foundDevice) {
+                        puts([[NSString stringWithFormat:@"Error: No video device matching '%@' found. Try -l for a list of available devices", deviceString] UTF8String]);
+                        exit(0);
+                    }
+                }
+                
                 [captureManager setCurrentVideoDevice:videoDevice];
                 [captureManager saveFrameToURL:[NSURL fileURLWithPath:outputFilename]];
+                NSRunLoop *theRL = [NSRunLoop currentRunLoop];
+                while (shouldKeepRunning && [theRL runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]]);
             }
- 
-            NSRunLoop *theRL = [NSRunLoop currentRunLoop];
-            while (shouldKeepRunning && [theRL runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]]);
         }
         
     }
