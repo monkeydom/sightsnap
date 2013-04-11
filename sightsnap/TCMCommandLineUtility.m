5ca218d4fa972df2fed540b32a64841ac8ca420f
Dominik Wagner <dom@codingmonkeys.de>
2013-04-11 21:54:12 +0200

    [BUMP] bumped version to 0.5 and added current download link

--- sightsnap/TCMCommandLineUtility.m
+++ sightsnap/TCMCommandLineUtility.m
@@ -348,7 +348,7 @@ typedef NSString * (^FSDescriptionHelper) (FSArgumentSignature *aSignature, NSUI
     
 	NSInteger terminalWidth = 80;
     if ([package booleanValueForSignature:help]) {
-		puts("sightsnap v0.4 by @monkeydom");
+		puts("sightsnap v0.5 by @monkeydom");
         puts("usage: sightsnap [options] [output[.jpg|.png]] [options]");
 		puts("");
 		puts("Default output filename is signtsnap.jpg - if no extension is given, jpg is used.\nIf you add directory in front, it will be created.");
